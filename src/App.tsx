42438323ddc00afae715b2640e8568825064de73
pedrobth <pmerchiorattor@gmail.com>
2023-10-11 17:55:01 -0300

    chore: add bootstrap and header

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import Header from "./components/Header";
-
+import 'bootstrap/dist/css/bootstrap.css';
 function App() {
   return (
     <>
--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,8 +1,19 @@
-function Header(){
-  return(
-  <header>
-    <h1>Hello World in Header component</h1>
-  </header>
+function Header() {
+  return (
+    <header>
+      <nav className="navbar navbar-expand-md bg-body-tertiary">
+        <div className="container-fluid">
+          <h3 className="navbar navbar-expand-md bg-body-tertiary">AppName</h3>
+          <div className="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul className="navbar-nav me-auto mb-2 mb-md-0">
+              <li className="nav-item">
+                <a className="nav-link active" aria-current="page" href="#">Main Feature</a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </header>
   );
 }
 
